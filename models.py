c34df35d15c2c687413ee7c3bd44fa2a8889d022
0shean <sergey.zahovskiy@gmail.com>
2025-06-03 23:45:16 +0200

    .

--- models.py
+++ models.py
@@ -132,7 +132,8 @@ class BaseModel(nn.Module):
         target_seq = model_out['target']
 
         # --- use only the first GT frame for the loss ---
-        T_used = 1  # always one-step loss
+        T_used = self.config.target_seq_len   # 24
+
         pred_used = pred_seq[:, :T_used]  # (B,1,D)
         targ_used = target_seq[:, :T_used]  # (B,1,D)
 
